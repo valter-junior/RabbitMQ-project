44168d5b501ca0b19a9d7b500a46105cbcfb26a4
valterjunior <featcode@mercadocar.com.br>
2024-04-15 13:22:12 -0300

    Subscribe fixed.

--- subscribe/subscribe.go
+++ subscribe/subscribe.go
@@ -4,15 +4,16 @@ import (
 	"fmt"
 	"log"
 	"os"
-	"sync"
 	"time"
 
 	"github.com/streadway/amqp"
 )
 
 var (
-	pfc          = 0
-	messageCount = 0
+	pfc              = 50
+	messageCount     = 0
+	firstMessageTime = time.Now().Truncate(time.Second)
+	lastMessageTime  = time.Now().Truncate(time.Second)
 )
 
 func failOnError(err error, msg string) {
@@ -56,43 +57,59 @@ func main() {
 
 	failOnError(err, "Failed to register a consumer")
 
-	var wg sync.WaitGroup
+	ticker := time.NewTicker(30 * time.Second)
+	defer ticker.Stop()
+
+	messageReceived := make(chan bool)
 
 	go func() {
-		for range time.Tick(30 * time.Second) {
-			file, err := os.OpenFile("message_count.txt", os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0644)
-			failOnError(err, "Failed to open file")
 
-			_, err = file.WriteString(fmt.Sprintf("%v: Received %d messages in the last 30 seconds, Rate: %.2f msg/sec\n", time.Now(), messageCount, float64(messageCount)/30.0))
-			failOnError(err, "Failed to write to file")
-
-			err = file.Close()
-			failOnError(err, "Failed to close file")
-
-			log.Printf("Messages processed: %d", messageCount)
-			messageCount = 0
-
-		}
-
-	}()
-
-	go func() {
 		for d := range msg {
 			log.Printf("Received a message: %s", d.Body)
-
 			messageCount++
-
+			lastMessageTime = time.Now().Truncate(time.Second)
+			messageReceived <- true
 			err := d.Ack(false)
 			failOnError(err, "Failed to acknowledge message")
-
 		}
+
 	}()
 
-	wg.Wait()
+	go func() {
+		for {
+			select {
+			case <-ticker.C:
+				logStats()
+			case <-messageReceived:
+				if time.Since(lastMessageTime) > 30*time.Second {
+					logStats()
+				}
+			}
+		}
+	}()
 
 	log.Printf(" [*] Waiting for messages. To exit press CTRL+C")
 	forever := make(chan bool)
 
 	<-forever
+
+}
+
+func logStats() {
+
+	duration := int(lastMessageTime.Sub(firstMessageTime).Seconds())
+
+	file, err := os.OpenFile("message_count_50.txt", os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0644)
+	failOnError(err, "Failed to open file")
+
+	_, err = file.WriteString(fmt.Sprintf("%v: Received %d messages in %d, Rate: %.2f msg/sec\n", lastMessageTime, messageCount, duration, float64(messageCount)/float64(duration)))
+	failOnError(err, "Failed to write to file")
+
+	err = file.Close()
+	failOnError(err, "Failed to close file")
+
+	log.Printf("Messages processed: %d", messageCount)
+	messageCount = 0
+	firstMessageTime = lastMessageTime
 
 }
