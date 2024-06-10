b1a3405bc01c38a04680853cbf55d7a3e9cb5edd
valterjunior <featcode@mercadocar.com.br>
2024-06-10 15:56:29 -0300

    Triangular membership function fixed.

--- triangular/subscribe_triangular.go
+++ triangular/subscribe_triangular.go
@@ -10,7 +10,6 @@ import (
 )
 
 var (
-	//pfc              = 50
 	messageCount     = 0
 	firstMessageTime = time.Now().Truncate(time.Second)
 	lastMessageTime  = time.Now().Truncate(time.Second)
@@ -69,62 +68,47 @@ func fuzzyficationOutput(n float64) map[string]float64 {
 }
 
 func applyRules(e map[string]float64) ([]float64, []float64) {
-
 	mx := []float64{}
 	output := []float64{}
 
-	// Rule 1:  IF e = LARGEPOSITIVE THEN output = LARGEINCREASE
-	eR := e[LARGEPOSITIVE]
-
-	m1 := eR
+	// Rule 1: IF e = LARGEPOSITIVE THEN output = LARGEINCREASE
+	m1 := e[LARGEPOSITIVE]
 	o1 := getMaxOutput(LARGEINCREASE)
 	mx = append(mx, m1)
 	output = append(output, o1)
 
-	// Rule 2:  IF e = MEDIUMPOSITIVE THEN output = LARGEINCREASE
-	eR = e[MEDIUMPOSITIVE]
-
-	m2 := eR
+	// Rule 2: IF e = MEDIUMPOSITIVE THEN output = LARGEINCREASE
+	m2 := e[MEDIUMPOSITIVE]
 	o2 := getMaxOutput(LARGEINCREASE)
 	mx = append(mx, m2)
 	output = append(output, o2)
 
-	// Rule 3:  IF e = SMALLPOSITIVE THEN output = SMALLINCREASE
-	eR = e[SMALLPOSITIVE]
-
-	m3 := eR
+	// Rule 3: IF e = SMALLPOSITIVE THEN output = SMALLINCREASE
+	m3 := e[SMALLPOSITIVE]
 	o3 := getMaxOutput(SMALLINCREASE)
 	mx = append(mx, m3)
 	output = append(output, o3)
 
-	// Rule 4:  IF e = ZE THEN output = MAINTAIN
-	eR = e[ZERO]
-
-	m4 := eR
+	// Rule 4: IF e = ZERO THEN output = MAINTAIN
+	m4 := e[ZERO]
 	o4 := getMaxOutput(MAINTAIN)
 	mx = append(mx, m4)
 	output = append(output, o4)
 
-	// Rule 5:  IF e = SN THEN output = SMALLPC
-	eR = e[SMALLNEGATIVE]
-
-	m5 := eR
+	// Rule 5: IF e = SMALLNEGATIVE THEN output = SMALLDECREASE
+	m5 := e[SMALLNEGATIVE]
 	o5 := getMaxOutput(SMALLDECREASE)
 	mx = append(mx, m5)
 	output = append(output, o5)
 
-	// Rule 6:  IF e = LN THEN output = LARGEPC
-	eR = e[MEDIUMNEGATIVE]
-
-	m6 := eR
+	// Rule 6: IF e = MEDIUMNEGATIVE THEN output = LARGEDECREASE
+	m6 := e[MEDIUMNEGATIVE]
 	o6 := getMaxOutput(LARGEDECREASE)
 	mx = append(mx, m6)
 	output = append(output, o6)
 
-	// Rule 7:  IF e = EXTREMELYNEGATIVE THEN output = LARGEDECREASE
-	eR = e[LARGENEGATIVE]
-
-	m7 := eR
+	// Rule 7: IF e = LARGENEGATIVE THEN output = LARGEDECREASE
+	m7 := e[LARGENEGATIVE]
 	o7 := getMaxOutput(LARGEDECREASE)
 	mx = append(mx, m7)
 	output = append(output, o7)
@@ -134,9 +118,9 @@ func applyRules(e map[string]float64) ([]float64, []float64) {
 
 func getMaxOutput(s string) float64 {
 	r := 0.0
-	max := -20000.0
+	max := -20000.0 // Initialize to a sufficiently low number to ensure any higher value is chosen.
 
-	for i := -4.0; i <= 5.0; i += 1.0 {
+	for i := -4.0; i <= 4.0; i += 0.5 { // Decreased step size for more precision
 		v := fuzzyficationOutput(i)
 
 		if v[s] > max {
@@ -147,22 +131,22 @@ func getMaxOutput(s string) float64 {
 	return r
 }
 
-func centroidDeffuzification(mx, output []float64) float64 {
-
-	numerator := 0.0
-	denominator := 0.0
+func centroidDeffuzification(mx, output, importanceFactors []float64) float64 {
+	numerator, denominator := 0.0, 0.0
 
 	for i := 0; i < len(mx); i++ {
-		numerator = numerator + mx[i]*output[i]
-		denominator = denominator + mx[i]
+		adjustedOutput := output[i] * importanceFactors[i]
+		numerator += mx[i] * adjustedOutput
+		denominator += mx[i]
+		fmt.Printf("Membership: %.4f, Adjusted Output: %.4f, Product: %.4f\n", mx[i], adjustedOutput, mx[i]*adjustedOutput)
 	}
-	u := 0.0
+
+	fmt.Printf("Numerator: %.4f, Denominator: %.4f\n", numerator, denominator)
 	if denominator == 0 {
-		u = 1
-	} else {
-		u = numerator / denominator
+		fmt.Println("Warning: Denominator is zero, defaulting output to 1")
+		return 1 // You may choose a different default behavior
 	}
-	return u
+	return numerator / denominator
 }
 
 func Result(p ...float64) float64 {
@@ -181,7 +165,8 @@ func Result(p ...float64) float64 {
 	mx, output := applyRules(fuzzifiedSetError)
 
 	// Deffuzification
-	u := centroidDeffuzification(mx, output)
+	importanceFactors := []float64{1.5, 1.5, 1.0, 1.0, 0.5, 0.5, 0.3}
+	u := centroidDeffuzification(mx, output, importanceFactors)
 
 	fmt.Printf("Fuzzy Controller: %.2f\n", u)
 	return u
@@ -196,7 +181,7 @@ func main() {
 	failOnError(err, "Failed to open a channel")
 	defer ch.Close()
 
-	err = ch.Qos(100, 0, true)
+	err = ch.Qos(5, 0, true)
 	failOnError(err, "Failed to set QoS")
 
 	q, err := ch.QueueDeclare(
@@ -256,7 +241,7 @@ func main() {
 					rate := float64(messageCount) / float64(duration)
 
 					log.Printf("Rate: %.2f msg/sec", rate)
-					Result(20000, float64(rate))
+					Result(10000, float64(rate))
 
 					if duration == 0 {
 						continue
@@ -282,22 +267,3 @@ func main() {
 	<-forever
 
 }
-
-/*func logStats() {
-
-	duration := int(lastMessageTime.Sub(firstMessageTime).Seconds())
-
-	file, err := os.OpenFile("message_count.txt", os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0644)
-	failOnError(err, "Failed to open file")
-
-	_, err = file.WriteString(fmt.Sprintf("%v: Received %d messages in %d, Rate: %.2f msg/sec\n", lastMessageTime, messageCount, duration, float64(messageCount)/float64(duration)))
-	failOnError(err, "Failed to write to file")
-
-	err = file.Close()
-	failOnError(err, "Failed to close file")
-
-	log.Printf("Messages processed: %d", messageCount)
-	messageCount = 0
-	firstMessageTime = lastMessageTime
-
-}*/
